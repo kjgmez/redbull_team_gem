cce7e74477cd37f3a6018817ed9365b33988e6eb
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-28 17:52:03 +0000

    scraper make player method name

--- redbull_team/lib/redbull_team/scraper.rb
+++ redbull_team/lib/redbull_team/scraper.rb
@@ -5,6 +5,20 @@ class Scraper
   def get_players
     self.get_page_player.css('li.row')
   end
+  def make_player
+    self.get_players.collect do |players|
+      player = Player.new
+      player.name = players.css('div.player_info div.name a').children.first.text
+      #binding.pry
+    end
+  end
+# def print_players
+#   self.make_player
+#   Player.all.each  do |player|
+#     puts "name: #{player.name}"
+#     puts "position: #{player.position}"
+#   end
+# end
 end
 
 #Scraper.new.make_player
